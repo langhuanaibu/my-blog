d73f6ee5aba547efd883f116df02d8b9e643cc00
Aoitsuki <2157983631@qq.com>
2026-06-19 15:48:56 +0800

    restore homepage snow and texture code deleted by regex replacement

--- source/js/aoiblog-home.js
+++ source/js/aoiblog-home.js
@@ -114,6 +114,24 @@
     addPaperTexture();
   }
 
+  if (!isHome) return;
+
+  document.body.classList.add('aoiblog-home');
+  addSnowCanvas();
+
+  if (path !== '/') return;
+
+  var bannerText = document.querySelector('#banner .banner-text');
+  if (!bannerText || bannerText.querySelector('.home-avatar')) return;
+
+  var avatar = document.createElement('img');
+  avatar.className = 'home-avatar';
+  avatar.src = '/images/my-avatar.jpg';
+  avatar.alt = 'Aoitsuki';
+  avatar.loading = 'eager';
+
+  bannerText.insertBefore(avatar, bannerText.firstChild);
+
   // ---- Mobile TOC ----
   function initMobileToc() {
     var tocBody = document.getElementById('toc-body');
